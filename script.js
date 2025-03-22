d1cea5d5ada894fc641add8829500d9ed3a2766b
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-03-22 23:13:40 +0000

    add type for for typed input js

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Web Designer', 'Web Developer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
